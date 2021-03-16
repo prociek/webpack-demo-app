c2d9d3e5d4252c8c64709d9559a180897e42de39
Tomasz Procko <prociek4@gmail.com>
2021-03-16 20:46:51 +0100

    added webpack babel dev-server

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import { run } from "./app/app";
-import "./main.scss";
-import { AlertService } from "./app/alert.service";
-import { ComponentService } from "./app/component.service";
-const alertService = new AlertService();
-const componentService = new ComponentService();
-run(alertService, componentService);
-console.log("J");
+// import { run } from "./app/app";
+// import "./main.scss";
+// import { AlertService } from "./app/alert.service";
+// import { ComponentService } from "./app/component.service";
+// const alertService = new AlertService();
+// const componentService = new ComponentService();
+// run(alertService, componentService);
+
+const obj = {
+  name: "tomek",
+  year: 38,
+};
+
+const obj2 = { ...obj, developer: "not yet" };
+debugger
+console.log(obj2);
